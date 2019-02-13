254a8ff23c5f0cf0577ca3a85de2a85d4b1ac6e4
Gabriela Velasquez <46896688+GabrielaVelasquez06@users.noreply.github.com>
2019-02-13 09:54:00 -0600

    Main

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+	import java.util.ArrayList;
+	import java.util.Scanner;
+        public class Main {
+
+            public static void main(String[] args) {
+
+                MenuPrincipal menu = new MenuPrincipal();
+                menu.Menu();
+
+            }
+}
+
